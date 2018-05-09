44d2cf734c64e8b824234f9e6ba27acd17aba844
gongweibao <weibao.gong@gmail.com>
2018-05-09 11:53:52 +0000

    add server multi port

---
+++ grpc_test/s1_c1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#start sever:
+#./server 40001,40002,40003,40004 1
+
+#ip=192.168.16.28
+ip=127.0.0.1
+loop_time=300
+
+arr=(4 16 32 64 128 256 512 1024 2048)
+for i in "${arr[@]}"
+do
+    size=$((i*1024))
+    echo $size
+    ./client ${ip}:40001,${ip}:40002,${ip}:40003,${ip}:40004 $loop_time $size 4
+    sleep 1
+done
