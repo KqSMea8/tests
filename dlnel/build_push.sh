6b4bece702cb15568de8d02009f0f69e91bbeee8
gongweibao <weibao.gong@gmail.com>
2018-10-31 15:37:14 +0800

    add dlnel

---
+++ dlnel/build_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+image=bootstrapper:5000/gongweibao/distributed_paddle_ubuntu_grpc:1.1.0
+unset http_proxy https_proxy
+docker build --network=host --build-arg UBUNTU_MIRROR=http://ftp.sjtu.edu.cn/ubuntu/ . -t ${image}
+#docker build  . -t ${image}
+docker push ${image}
---
+++ dlnel/prepare.sh
@@ -0,0 +1,2 @@
+cp ~/go/src/github.com/PaddlePaddle/Paddle/build/build_1.0.0_release_gpu/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl ./
+cp ~/go/src/github.com/PaddlePaddle/models/fluid/PaddleNLP/neural_machine_translation/transformer/*.py transformer/
