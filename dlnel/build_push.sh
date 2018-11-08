2e6fc0474bb2438a40f781c843bce53a8079d655
chenjiawen <chenjiawen@baidu.com>
2018-11-08 17:15:34 +0800

    add

--- dlnel/build_push.sh
+++ dlnel/build_push.sh
@@ -2,7 +2,7 @@
 set -xe
 
 image=bootstrapper:5000/gongweibao/distributed_paddle_ubuntu_grpc:1.1.0
+
 unset http_proxy https_proxy
 docker build --network=host --build-arg UBUNTU_MIRROR=http://ftp.sjtu.edu.cn/ubuntu/ . -t ${image}
-#docker build  . -t ${image}
-docker push http://${image}
+docker push ${image}
--- dlnel/prepare.sh
+++ dlnel/prepare.sh
@@ -2,5 +2,14 @@
 set -e
 
 rm -f whl/*.whl
-cp ~/go/src/github.com/PaddlePaddle/Paddle/build/build_1.1.0_Release_gpu/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl whl/
+
+#whl
+#cp ~/go/src/github.com/PaddlePaddle/Paddle/build/build_1.1.0_Release_gpu/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl whl/
+#cp ~/go/src/github.com/PaddlePaddle/Paddle/build/build_nccl2test_RelWithDebInfo_gpu/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl whl/
+cp ~/go/src/github.com/PaddlePaddle/Paddle/build/build_exectest_RelWithDebInfo_gpu/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl whl/
+#cp ~/go/src/github.com/PaddlePaddle/Paddle/build/build_fcf370e_RelWithDebInfo_gpu/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl whl/
+#cp ~/go/src/github.com/PaddlePaddle/Paddle/build/build_develop_RelWithDebInfo_gpu/python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl whl/
+cp ~/go/src/github.com/PaddlePaddle/Paddle/build/third_party/RelWithDebInfo_gpu/install/mkldnn/lib/libmkldnn.so.0 .
+
+#models
 cp ~/go/src/github.com/PaddlePaddle/models/fluid/PaddleNLP/neural_machine_translation/transformer/*.py models/
