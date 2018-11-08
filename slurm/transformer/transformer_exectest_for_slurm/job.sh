d94e1356e9d772b738011b0ab61c5646ba428af4
gongweibao <weibao.gong@gmail.com>
2018-11-08 17:53:53 +0800

    add

--- slurm/transformer/transformer_exectest_for_slurm/job.sh
+++ slurm/transformer/transformer_exectest_for_slurm/job.sh
@@ -3,12 +3,5 @@ set -x
 
 mpirun hostname
 
-echo "start"
-
 iplist=`cat nodelist-${SLURM_JOB_ID} | xargs  | sed 's/ /,/g'`
-mpirun --bind-to none -x iplist=${iplist} sh test_nccl.sh transformer 3200 8
-
-#mpirun --bind-to none -x iplist=${iplist} sh test_nccl.sh resnet 32 8
-#mpirun --bind-to none -x iplist=${iplist} sh test_pserver.sh resnet 32 8 &> log/resnet_32_8.txt
-#mpirun --bind-to none sh test_local.sh resnet 32 8 &> log/resnet_32_8.txt
-
+mpirun --bind-to none -x iplist=${iplist} sh test_transformer_nccl.sh
---
+++ slurm/transformer/transformer_exectest_for_slurm/test_transformer_nccl.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -xe
+
+. env.sh
+
+export PADDLE_IS_LOCAL=0
+export FLAGS_fraction_of_gpu_memory_to_use=1.0
+
+python -u train.py \
+    --src_vocab_fpath 'dataset/cluster_test_data_en_fr/thirdparty/vocab.wordpiece.en-fr' \
+    --trg_vocab_fpath 'dataset/cluster_test_data_en_fr/thirdparty/vocab.wordpiece.en-fr' \
+    --special_token '<s>' '<e>' '<unk>'  \
+    --token_delimiter '\x01' \
+    --train_file_pattern 'dataset/cluster_test_data_en_fr/train/train.wordpiece.en-fr.0' \
+    --val_file_pattern 'dataset/cluster_test_data_en_fr/thirdparty/newstest2014.wordpiece.en-fr' \
+    --use_token_batch True \
+    --batch_size  3200 \
+    --sort_type pool \
+    --pool_size 200000 \
+    --local False \
+  --shuffle True \
+  --shuffle_batch True \
+  --use_mem_opt True \
+  --enable_ce False \
+  --update_method nccl2 \
+  d_model 1024 \
+  d_inner_hid 4096 \
+  n_head 16 \
+  learning_rate 2.0 \
+  warmup_steps 8000 \
+  beta2 0.997 \
+  pass_num 100 \
+  prepostprocess_dropout 0.3 \
+  attention_dropout 0.1 \
+  relu_dropout 0.1 \
+  weight_sharing True > log/${PADDLE_TRAINER_ID}.txt 2>&1 
