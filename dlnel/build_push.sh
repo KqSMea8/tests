6c5c3d1f5a89a7001735c48d0cf57722a0a20149
gongweibao <weibao.gong@gmail.com>
2018-11-09 14:02:24 +0800

    add

--- dlnel/build_push.sh
+++ dlnel/build_push.sh
@@ -6,3 +6,7 @@ image=bootstrapper:5000/gongweibao/distributed_paddle_ubuntu_grpc:1.1.0
 unset http_proxy https_proxy
 docker build --network=host --build-arg UBUNTU_MIRROR=http://ftp.sjtu.edu.cn/ubuntu/ . -t ${image}
 docker push ${image}
+
+bd_image=registry.baidu.com/gongweibao/distributed_paddle_ubuntu_grpc:exectest
+docker tag ${image} ${bd_image}
+docker push ${bd_image}
--- slurm/transformer/transformer_exectest_for_slurm/test_transformer_local.sh
+++ slurm/transformer/transformer_exectest_for_slurm/test_transformer_local.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-export PATH=$PWD/python/bin/:$PATH
+hadoop fs -Dhadoop.job.ugi=bml,bml#user \
+    -fs hdfs://nmg01-mulan-hdfs.dmop.baidu.com:54310 \
+    -get /app/inf/mpi/bml-guest/paddlepaddle/public/transformer/cluster_test_data_en_fr .
 
-export LD_LIBRARY_PATH=/home/work/cuda-9.0/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/work/cudnn/cudnn_v7/cuda/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$PWD/nccl_2.3.5/lib/:$LD_LIBRARY_PATH
+. ./env.sh
 
-export PADDLE_DATASET_DIR=$PWD/dataset/
+export PADDLE_IS_LOCAL=1
+export FLAGS_fraction_of_gpu_memory_to_use=1.0
 
-model=$1
-bsize=$2
-gpus=$3
-
-python fluid_benchmark.py \
-        --no_test      \
-        --model $model \
-        --batch_size $bsize \
-        --pass_num 10 \
-        --iterations 30 \
-        --device GPU \
-        --gpus $gpus \
+python -u train.py \
+    --src_vocab_fpath 'cluster_test_data_en_fr/thirdparty/vocab.wordpiece.en-fr' \
+    --trg_vocab_fpath 'cluster_test_data_en_fr/thirdparty/vocab.wordpiece.en-fr' \
+    --special_token '<s>' '<e>' '<unk>'  \
+    --token_delimiter '\x01' \
+    --train_file_pattern 'cluster_test_data_en_fr/train/train.wordpiece.en-fr.0' \
+    --val_file_pattern 'cluster_test_data_en_fr/thirdparty/newstest2014.wordpiece.en-fr' \
+    --use_token_batch True \
+    --batch_size  3200 \
+    --sort_type pool \
+    --pool_size 200000 \
+  --shuffle True \
+  --shuffle_batch True \
+  --use_mem_opt True \
+  --enable_ce False \
+  --update_method nccl2 \
+  d_model 1024 \
+  d_inner_hid 4096 \
+  n_head 16 \
+  learning_rate 2.0 \
+  warmup_steps 8000 \
+  beta2 0.997 \
+  pass_num 100 \
+  prepostprocess_dropout 0.3 \
+  attention_dropout 0.1 \
+  relu_dropout 0.1 \
+  weight_sharing True > log/local.txt 2>&1 
